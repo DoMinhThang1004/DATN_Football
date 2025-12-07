fba79f8ce0e2f5fa7558a8795305e29a94c11614
Do Minh Thang <dominhthang101004@gmail.com>
2025-12-08 00:23:11 +0700

    fix db.js

--- server/db.js
+++ server/db.js
@@ -1,19 +1,31 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
+// sử dụng biến DATABASE_URL khi triển khai,k thì về cấu hình cục bộ
+const connectionConfig = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      // cấu hìh ssl
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  : {
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+    };
+
+const pool = new Pool(connectionConfig);
 
 pool.connect((err) => {
   if (err) {
     console.error('Lỗi kết nối PostgreSQL:', err.message);
   } else {
-    console.log('Đã kết nối thành công đến PostgreSQL: ' + process.env.DB_NAME);
+    // debug kiểmt ra
+    console.log('Đã kết nối thành công đến Database Cloud!');
   }
 });
 
